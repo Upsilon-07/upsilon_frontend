b1be7008946f46d706f1938de2068d94ab466bf5
Laura Carvalho <115020549+lauratlcarvalho@users.noreply.github.com>
2023-08-25 13:51:34 +0100

    Made homePageButton change color when in homepage
    Made homePageButton change color when in homepage

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,28 @@
 import './Navbar.css'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import profileButtonGrey from '/src/assets/images/navbar/profile_page_button_grey.svg'
 import homepageButtonGrey from '/src/assets/images/navbar/home_button_grey.svg'
+import homepageButtonOrange from '/src/assets/images/navbar/home_button_orange.svg'
 import coursesButtonGrey from '/src/assets/images/navbar/courses_button_grey.svg'
 import relaxingMusicButtonGrey from '/src/assets/images/navbar/relaxing_music_button_grey.svg'
 import mealsButtonGrey from '/src/assets/images/navbar/meals_button_grey.svg'
 
 
 const Navbar = () => {
+
+const location = useLocation()
+
   return (
-    <div className='navbar'>
-    <div className='navbar-container'>
+    <nav className='navbar'>
+    <ul className='navbar-container'>
         <Link to='/'>
-        <img src={homepageButtonGrey} alt="button to go to homepage" /></Link> 
-        <img src={coursesButtonGrey} alt="button to go to courses page"/>
+        <img src={location.pathname === "/" ? homepageButtonOrange : homepageButtonGrey} alt="button to go to homepage" /></Link> 
+        <img src={location.pathname.includes('/courses') ? homepageButtonOrange : coursesButtonGrey} alt="button to go to courses page"/>
         <img src={relaxingMusicButtonGrey} alt="button to go to relaxing music page"/>
         <img src={mealsButtonGrey} alt="button to go to meals page"/>
         <img src={profileButtonGrey} alt="button to go to user profile"/>
-    </div>
-    </div>
+    </ul>
+    </nav>
   )
 }
 
