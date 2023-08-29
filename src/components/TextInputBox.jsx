6f601fe33e4c56c550cc268a10af6f492daf060b
Goncalo Amaro <goncalo.amaro7@gmail.com>
2023-08-29 17:36:57 +0100

    upload the structure of the page and the component

--- src/components/TextInputBox.jsx
+++ src/components/TextInputBox.jsx
@@ -2,7 +2,6 @@ import PropTypes from "prop-types";
 function TextInputBox({ data }) {
   return (
     <div className="text-input">
-      <label>{data.label}</label>
       <input
         type={data.type}
         placeholder={data.placeholder}
@@ -15,11 +14,10 @@ function TextInputBox({ data }) {
 
 TextInputBox.propTypes = {
   data: PropTypes.shape({
-    label: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
     placeholder: PropTypes.string.isRequired,
     value: PropTypes.string.isRequired,
-    onChange: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
   }).isRequired,
 };
 
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,17 +1,49 @@
 import NextButton from "../../components/next-page-button/NextButton";
 import TextInputBox from "../../components/TextInputBox";
 import Title from "../../components/Title";
+import { useState } from "react";
 function RegisterPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
   const Data = {
     title: "Resgister",
-    buttonContent: 'Next',
+    buttonContent: "Next",
+    buttonId: "orange-button",
+    type: "email",
+    placeholder: "enter your email",
+    value: email,
+    onChange: handleEmailChange,
   };
+  const DataTwo = {
+
+    type: "password",
+    placeholder: "enter your password",
+    value: password,
+    onChange: handlePasswordChange,
+  };
+//   const DataThree = {
+//     type: "email",
+//     placeholder: "enter your email",
+//     value: email,
+//     onChange: handleEmailChange,
+//   };
   return (
     <>
       <div>
         <Title data={Data} />
         <TextInputBox data={Data} />
+        <TextInputBox data={DataTwo} />
+        {/* <TextInputBox data={DataThree} /> */}
         <NextButton data={Data} />
+
       </div>
     </>
   );
