0ca3d1a3bf173cfc9c7d00724cd87da1d5d75d87
Laura Carvalho <115020549+lauratlcarvalho@users.noreply.github.com>
2023-08-29 18:39:33 +0100

    Added text input boxes and styled the Profile page
    Added text input boxes with the TextInputBox.jsx component and continued styling the Profile page

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -4,14 +4,48 @@ import Title from "../../components/Title";
 import Navbar from "../../components/navbar/Navbar";
 import NextButton from "../../components/next-page-button/NextButton";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import TextInputBox from "../../components/TextInputBox";
 
 const ProfilePage = () => {
   const id = 2;
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  
+  const handleUserNameChange = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const DataOne = {
+    type: "text",
+    value: userName,
+    onChange: handleUserNameChange,
+  };
+
+  const DataTwo = {
+    type: "email",
+    value: email,
+    onChange: handleEmailChange,
+  };
+  
   return (
     <div className="profile-page">
+        <div>
+            
+        </div>
       <div className="profile-title">
         <Title data={homePageData.find((data) => data.id === id)} />
       </div>
+
+        <div className="profile-input-box">
+        <div className="profile-input-box1"><TextInputBox data={DataOne} /></div>
+        <div className="profile-input-box2"><TextInputBox data={DataTwo} /></div>
+        </div>
+
       <div className="profile-button" id="profile-edit">
         <Link to="/user-profile/edit-profile">
           <NextButton data={homePageData.find((data) => data.id === id)} />
