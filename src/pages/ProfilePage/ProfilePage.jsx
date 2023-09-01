6b1f32d7de2c963c226998365044bff6b4167922
Laura Carvalho <115020549+lauratlcarvalho@users.noreply.github.com>
2023-09-01 13:43:01 +0100

    Changed the components of the ProfilePage
    Changed the components of the ProfilePage due to changes in the components

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,66 +1,59 @@
 import "./ProfilePage.css";
-import { homePageData } from "../../assets/HomePage/HomePageData";
 import Title from "../../components/Title";
 import Navbar from "../../components/navbar/Navbar";
 import NextButton from "../../components/next-page-button/NextButton";
 import { Link } from "react-router-dom";
-import { useState } from "react";
 import TextInputBox from "../../components/TextInputBox";
 import ProfilePicture from "../../components/ProfilePicture/ProfilePicture";
+import NavbarDesktop from "../../components/NavbarDesktop/NavbarDesktop";
 
 const ProfilePage = () => {
-  const id = 2;
-  const [userName, setUserName] = useState("");
-  const [email, setEmail] = useState("");
-  
-  const handleUserNameChange = (event) => {
-    setUserName(event.target.value);
-  };
-
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const DataOne = {
-    type: "text",
-    value: userName,
-    onChange: handleUserNameChange,
-  };
-
-  const DataTwo = {
-    type: "email",
-    value: email,
-    onChange: handleEmailChange,
-  };
+ 
 
   return (
     <div className="profile-page">
-        <div>
-            
-        </div>
+    <NavbarDesktop />
       <div className="profile-title">
-        <Title data={homePageData.find((data) => data.id === id)} />
+      <Title title="Profile" />
         <ProfilePicture/>
       </div>
 
         <form className="profile-input-box">
-        <div className="profile-input-box1"><TextInputBox data={DataOne} /></div>
-        <div className="profile-input-box2"><TextInputBox data={DataTwo} /></div>
+        <label>username:</label>
+          <TextInputBox
+            label="Username"
+            type="username"
+            name="username"
+          />
+          <label>email:</label>
+          <TextInputBox
+            label="Email"
+            type="email"
+            name="email"
+          />
         </form>
 
       <div className="profile-button" id="profile-edit">
         <Link to="/user-profile/edit-profile">
-          <NextButton data={homePageData.find((data) => data.id === id)} />
+        <NextButton
+            buttonId="orange-button"
+            buttonContent="EDIT PROFILE"
+            buttonClass="button-square"
+          />
         </Link>
       </div>
       <div className="profile-button" id="change-password">
         <Link to="/user-profile/change-password">
-          <NextButton data={homePageData.find((data) => data.id === id + 1)} />
+        <NextButton
+            buttonId="white-button"
+            buttonContent="CHANGE PASSWORD"
+            buttonClass="button-square"
+          />
         </Link>
       </div>
       <Navbar />
     </div>
   );
-};
+}
 
 export default ProfilePage;
