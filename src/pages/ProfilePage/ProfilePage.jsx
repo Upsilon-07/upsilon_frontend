f36fefdc1b9776aadb6d39a7b85b1e2cfd3f3d26
Laura Carvalho <115020549+lauratlcarvalho@users.noreply.github.com>
2023-09-01 14:35:53 +0100

    Did style changes to the ProfilePage
    Did style changes to the ProfilePage

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -3,39 +3,29 @@ import Title from "../../components/Title";
 import Navbar from "../../components/navbar/Navbar";
 import NextButton from "../../components/next-page-button/NextButton";
 import { Link } from "react-router-dom";
-import TextInputBox from "../../components/TextInputBox";
 import ProfilePicture from "../../components/ProfilePicture/ProfilePicture";
 import NavbarDesktop from "../../components/NavbarDesktop/NavbarDesktop";
 
 const ProfilePage = () => {
- 
-
   return (
     <div className="profile-page">
-    <NavbarDesktop />
+      <NavbarDesktop />
       <div className="profile-title">
-      <Title title="Profile" />
-        <ProfilePicture/>
+        <Title title="Profile" weight={"light-title"} />
+        <ProfilePicture />
       </div>
 
-        <form className="profile-input-box">
-        <label>username:</label>
-          <TextInputBox
-            label="Username"
-            type="username"
-            name="username"
-          />
-          <label>email:</label>
-          <TextInputBox
-            label="Email"
-            type="email"
-            name="email"
-          />
-        </form>
+      <form className="profile-input-box">
+        <label>User name:</label>
+        <input type="text" className="profile-input-box1" name="user_name" />
 
-      <div className="profile-button" id="profile-edit">
+        <label>E-mail:</label>
+        <input type="email" className="profile-input-box2" name="email" />
+      </form>
+
+      <div className="profile-button" id="profile-edit-button">
         <Link to="/user-profile/edit-profile">
-        <NextButton
+          <NextButton
             buttonId="orange-button"
             buttonContent="EDIT PROFILE"
             buttonClass="button-square"
@@ -44,7 +34,7 @@ const ProfilePage = () => {
       </div>
       <div className="profile-button" id="change-password">
         <Link to="/user-profile/change-password">
-        <NextButton
+          <NextButton
             buttonId="white-button"
             buttonContent="CHANGE PASSWORD"
             buttonClass="button-square"
@@ -54,6 +44,6 @@ const ProfilePage = () => {
       <Navbar />
     </div>
   );
-}
+};
 
 export default ProfilePage;
