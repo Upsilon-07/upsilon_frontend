b045464ddbac85a683ad7da5f20c3fcc094dce86
Laura Carvalho <115020549+lauratlcarvalho@users.noreply.github.com>
2023-09-04 20:47:28 +0100

    Updated the UserName component
    Updated the UserName component

--- src/components/UserName/UserName.jsx
+++ src/components/UserName/UserName.jsx
@@ -1,19 +1,53 @@
-//import { useState } from "react"
+import { useEffect, useState } from "react"
 import './UserName.css'
+import PropTypes from 'prop-types';
+import api from "../../api/api";
 
-const UserName = () => {
- //   const [ profileName, setProfileName ] = useState()
+const UserName = ({ userId }) => {
+   const [user, setUser] = useState(null);
+
+   useEffect(() => {
+      api
+        .get(`/users/${userId}`)
+        .then((response) => {
+          if (response.status === 200) {
+            console.log(response);
+            setUser(response.data.username);
+          } else {
+            console.log("Error getting the username");
+          }
+        })
+        .catch((error) => {
+          console.error('Error fetching the user', error);
+        });
+    }, [userId]);
+   
+
   return (
     <>
     <div>
         <h1 className="homepage-title1">Namaste,</h1>
         </div>
     <div className="homepage-title2">
-      Vani
+      {user ? (
+        <h2 className="username-display">{user.username}</h2>) : (<p>Loading user data...</p>)}
     </div>
     </>
-  )
-}
+  );
+};
+
+UserName.PropTypes = {
+  userId: PropTypes.shape,
+  data: PropTypes.shape({
+    id: PropTypes.string,
+    username: PropTypes.string,
+  }),
+  userName: PropTypes.shape({
+    id: PropTypes.string,
+    username: PropTypes.string,
+  }),
+};
+
 
 export default UserName
 
