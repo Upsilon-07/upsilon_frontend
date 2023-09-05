0728097603d1790055303ed39f34de1d0b392f99
Laura Carvalho <115020549+lauratlcarvalho@users.noreply.github.com>
2023-09-05 20:30:34 +0100

    Created ExerciseCard component
    Created ExerciseCard component

---
+++ src/components/ExerciseCard/ExerciseCard.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+
+const ExerciseCard = () => {
+const [exerciseCount, setExerciseCount] = useState
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ExerciseCard
