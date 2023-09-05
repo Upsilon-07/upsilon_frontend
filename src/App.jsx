6c0ed87f34082fe159ab8d5ad569aafadc7479d5
Goncalo Amaro <goncalo.amaro7@gmail.com>
2023-09-05 17:39:57 +0100

    finilazedlogin page front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import './App.css';
-import CourseFavouritePage from './pages/CourseFavouritePage/CourseFavouritePage';
-import CourseLessonPage from './pages/CourseLessonPage/CourseLessonPage';
-import CoursePage from './pages/CoursePage/CoursePage';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage'
-import StartJourney from './pages/Start-Journey-Page/StartJourney'
-import StartJourneyTwo from './pages/Start-Journey-Page/StartJourneyTwo';
-import StartJourneyThree from './pages/Start-Journey-Page/StartJourneyThree';
-import StartJourneyFour from './pages/Start-Journey-Page/StartJourneyFour';
-import ProfilePage from './pages/ProfilePage/ProfilePage';
-import RegisterPage from './pages/RegisterPage/RegisterPage';
+import "./App.css";
+import CourseFavouritePage from "./pages/CourseFavouritePage/CourseFavouritePage";
+import CourseLessonPage from "./pages/CourseLessonPage/CourseLessonPage";
+import CoursePage from "./pages/CoursePage/CoursePage";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import StartJourney from "./pages/Start-Journey-Page/StartJourney";
+import StartJourneyTwo from "./pages/Start-Journey-Page/StartJourneyTwo";
+import StartJourneyThree from "./pages/Start-Journey-Page/StartJourneyThree";
+import StartJourneyFour from "./pages/Start-Journey-Page/StartJourneyFour";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import ExercisesPage from "./pages/ExercisesPage/ExercisesPage";
 import AuthContext from "./contexts/AuthContext";
@@ -22,32 +22,30 @@ function App() {
   const { isAuthenticated } = useContext(AuthContext);
   return (
     <>
-
       <Routes>
+        <Route path="/start-journey" element={<StartJourney />} />
+        <Route path="/start-journey-1" element={<StartJourneyTwo />} />
+        <Route path="/start-journey-2" element={<StartJourneyThree />} />
+        <Route path="/start-journey-3" element={<StartJourneyFour />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+
         <Route
-           element={
-             <ProtectedRoute redirectPath="/login" isAllowed={isAuthenticated && user} />
-           }
-           >
-        <Route path='/' element={<HomePage/>} />
-        <Route path='/user-profile' element={<ProfilePage/>} />
-        {/* <Route path='/user-profile/edit-profile' element={<EditProfilePage/>} />
-        <Route path='/user-profile/change-password' element={<ChangePasswordPage/>} /> */}
-        <Route path="/courses" element={<CoursePage/>} />
-        <Route path="/courses/favourite" element={<CourseFavouritePage/>} />
-        <Route path="/courses/:id" element={<CourseLessonPage/>} />
-        <Route path="/exercises/:id" element={<ExercisesPage />} />
-        <Route path='/start-journey' element={<StartJourney/>}/>
-         </Route>
-
-
-      <Route path='/start-journey-1' element={<StartJourneyTwo/>}/>
-      <Route path='/start-journey-2' element={<StartJourneyThree/>}/>
-      <Route path='/start-journey-3' element={<StartJourneyFour/>}/>
-      <Route path='/register' element={<RegisterPage/>}/>
-      <Route path='/login' element={<LoginPage/>}/>
+          element={
+            <ProtectedRoute
+              redirectPath="/login"
+              isAllowed={isAuthenticated && user}
+            />
+          }
+        >
+          <Route index element={<HomePage />} />
+          <Route path="/user-profile" element={<ProfilePage />} />
+          <Route path="/courses" element={<CoursePage />} />
+          <Route path="/courses/favourite" element={<CourseFavouritePage />} />
+          <Route path="/courses/:id" element={<CourseLessonPage />} />
+          <Route path="/exercises/:id" element={<ExercisesPage />} />
+        </Route>
       </Routes>
-   
     </>
   );
 }
