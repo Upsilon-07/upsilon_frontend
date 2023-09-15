8fab96363506c153e1659a3b8e20b2914e2571ee
Laura Carvalho <115020549+lauratlcarvalho@users.noreply.github.com>
2023-09-15 14:16:20 +0100

    Styled NutrientsCircle component

--- src/components/NutrientsCircle/NutrientsCircle.jsx
+++ src/components/NutrientsCircle/NutrientsCircle.jsx
@@ -1,11 +1,10 @@
-import "./NutrientsCircle.css"
+import "./NutrientsCircle.css";
 import { PropTypes } from "prop-types";
 
-function NutrientsCircle({data}) {
+function NutrientsCircle({ data }) {
   // Filter the nutrients for Protein, Carbohydrates, and Fat
-  const macroNutrients = data.filter(
-    (nutrient) =>
-      ["Protein", "Carbohydrates", "Fat"].includes(nutrient.nutrient_name)
+  const macroNutrients = data.filter((nutrient) =>
+    ["Protein", "Carbohydrates", "Fat"].includes(nutrient.nutrient_name)
   );
 
   // Find the nutrient with the name 'Calories'
@@ -21,39 +20,45 @@ function NutrientsCircle({data}) {
 
   return (
     <div>
-        <h2 className="nutrient-card-title">{`Nutrition`}</h2>
-        <div className="nutrient-circles">
-      {macroNutrients.map((nutrient, index) => (
-        <div className="stance-circle" key={index}>
-          <svg width="70" height="70">
-            <circle className={`meter-1`} cx="50" cy="50" r="40" />
-            <text
-              x="37%"
-              y="49%"
-              dominantBaseline="middle"
-              textAnchor="middle"
-              fill="rgba(170, 170, 170, 0.70)"
-            >
-              {nutrient.nutrient_name === "Carbohydrates" ? "Carbs" : nutrient.nutrient_name}
-            </text>
-            <text
-              x="37%"
-              y="66%"
-              dominantBaseline="middle"
-              textAnchor="middle"
-              fill="#EB784E"
-            >
-              {`${((nutrient.nutrient_value / totalMacroValue) * 100).toFixed(2)}%`}
-            </text>
-          </svg>
-          <div className="stance-bottom">
-            <p className="stance-performance">
-              {nutrient.nutrient_name === "Carbohydrates" ? "Carbs" : nutrient.nutrient_name}:{" "}
-              {String(nutrient.nutrient_value)} g
-            </p>
+      <h2 className="nutrient-card-title">{`Nutrition`}</h2>
+      <div className="nutrient-circles">
+        {macroNutrients.map((nutrient, index) => (
+          <div className="stance-circle" key={index}>
+            <svg width="70" height="70">
+              <circle className={`meter-1`} cx="50" cy="50" r="40" />
+              <text
+                x="37%"
+                y="49%"
+                dominantBaseline="middle"
+                textAnchor="middle"
+                fill="rgba(170, 170, 170, 0.70)"
+              >
+                {nutrient.nutrient_name === "Carbohydrates"
+                  ? "Carbs"
+                  : nutrient.nutrient_name}
+              </text>
+              <text
+                x="37%"
+                y="66%"
+                dominantBaseline="middle"
+                textAnchor="middle"
+                fill="#EB784E"
+              >
+                {`${((nutrient.nutrient_value / totalMacroValue) * 100).toFixed(
+                  2
+                )}%`}
+              </text>
+            </svg>
+            <div className="stance-bottom">
+              <p className="stance-performance">
+                {nutrient.nutrient_name === "Carbohydrates"
+                  ? "Carbs"
+                  : nutrient.nutrient_name}
+                : {String(nutrient.nutrient_value)} g
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
       </div>
       <p className="stance-description">
         {caloriesNutrient
@@ -74,4 +79,4 @@ NutrientsCircle.propTypes = {
   ).isRequired,
 };
 
-export default NutrientsCircle
+export default NutrientsCircle;
